abeef618c2b273b5e86ba79938fa2472faaf91a6
abuzarsalman <abuzarsalman484@gmail.com>
2023-04-07 00:03:12 -0700

    kuch code added

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const meriFileSetting = multer.diskStorage({
 
             });
         }else{
-            cb({message:"USer already h "}, null);
+            cb({code:100, message:"USer already h "}, null);
         }
 
 
@@ -43,6 +43,9 @@ const meriFileSetting = multer.diskStorage({
   })
   
   const upload = multer({ storage: meriFileSetting })
+
+//   middle-wares
+//   
 
 // request-response system
 // REST API
@@ -55,12 +58,49 @@ let meriApp = myExpress();
 // DELETE, delete data
 
 meriApp.use(myExpress.json());
+
 let users = [];
 
 
 // route ki 2 types
 // UI routes
 // data routes
+
+
+// meriApp.use(function(req, res, cb){
+//     if(req.query.roll == undefined){
+//         res.end("roll number provide karen")
+//     }else{
+//         cb();
+//     }
+// })
+
+// meriApp.get('/result/:roll', function(req, res){
+//     console.log("yeh roll number h "+req.params.roll)
+//     res.end("result agya "+req.params.roll)
+// })
+
+// meriApp.get('/cat', function(req, res){
+//     res.end("code chaling");
+// })
+
+
+// meriApp.use(function(err, req, res, cb){
+//     res.end("aagey nahi jana")
+// })
+
+// meriApp.get('/checking', function(req, res, cb){
+
+//     cb();
+
+// }, function(req, res, cb){
+
+//     cb();
+
+// }, function(req, res){
+//     console.log("code chaling ow");
+//     res.end("code chlya wa");
+// })
 
 meriApp.post('/session-check', async (req, res)=>{
 
@@ -107,7 +147,7 @@ meriApp.put('/user-update', function(req, res){
 
     let user = users[userIndex];
 
-    fs.renameSync('./my-uploads/'+user.name, './my-uploads/'+req.body.name);
+    fs.renameSync('./my-uploads/'+user.name, './server/my-uploads/'+req.body.name);
 
 
     users[userIndex] = req.body;
@@ -118,7 +158,7 @@ meriApp.put('/user-update', function(req, res){
 
 })
 
-meriApp.get('/user-lao', function(req, res){
+meriApp.get('/get-user-by-id', function(req, res){
 
    let userMilgya = users.find(user=> user.id == req.query.id);
    res.json(userMilgya);
@@ -142,9 +182,9 @@ meriApp.delete('/user-delete', function(req, res){
 
 });
 
-meriApp.post('/create-user', upload.array('pic',20), function(req, res){
+meriApp.post('/create-user', function(req, res){
 
-    req.body.pic = req.files[0].originalname;
+    // req.body.pic = req.files[0].originalname;
 
     users.push(req.body);
     console.log(req.body)
@@ -159,21 +199,39 @@ meriApp.get('/abc', function(req, res){
     // res.end("m5 is chaling")
 })
 
-meriApp.get('/abc', function(reqeust, response){
-    response.json({
-        name:"khurram",
-        city:"FSD"
-    });
-    // response.sendfile('./server/data.jpg');
-    // response.end("haha")
-});
+// meriApp.get('/abc', function(reqeust, response){
+//     response.json({
+//         name:"khurram",
+//         city:"FSD"
+//     });
+//     // response.sendfile('./server/data.jpg');
+//     // response.end("haha")
+// });
 
 meriApp.use(myExpress.static('./build'))
 meriApp.use(myExpress.static('./my-uploads'));
 
 meriApp.use(function(err, req, res, cb){
-    res.status(500).json(err);
+
+    switch(err.code){
+
+        case 100:
+            res.json({
+                message:"USER ALERADY EXIST",
+                code:300,
+                helpingCode:299
+            })
+            break;
+
+    }
+   
 })
+
+meriApp.get('*', function(req, res){
+
+    res.sendfile('./build/index.html');
+
+});
 
 meriApp.listen(3002, function(){
     console.log("server chaling now");
